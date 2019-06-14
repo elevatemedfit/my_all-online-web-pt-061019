00741dda0d64651d916b7dfda7d930190d429037
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-14 14:06:46 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -6,7 +6,7 @@ def my_all?(collection)
   collection.each do |i|
     while i < collection.length
     nuarray  << yield(collection[i]-1)
-    i +=1 #i += 1 does the same thing. Use this if it's easier for you.
+    i = i + 1 #i += 1 does the same thing. Use this if it's easier for you.
     end
 nuarray
 end
