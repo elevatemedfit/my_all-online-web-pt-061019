62d8f7859ee1c522295e84e0dc42a8a2198eecb6
poignant-firmware-1783 <elevatemedfit@gmail.com>
2019-06-14 13:57:12 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,13 @@
 require 'pry'
 
 def my_all?(collection)
-
+  nuarray =[]
+  i = 0
+  collection.each do |i|
+    while i < collection.length
+    nuarray  <<(collection[i])
+    i +=1 #i += 1 does the same thing. Use this if it's easier for you.
+    end
+    nuarray
+end
 end
